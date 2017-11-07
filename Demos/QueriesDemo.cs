16271f0d613c528ff18cdc2ae32a615772aa2029
Marco Parenzan <marco.parenzan@live.it>
2017-11-07 08:47:37 +0100

    Initial Commit

---
+++ Demos/QueriesDemo.cs
@@ -0,0 +1,41 @@
+﻿using DocumentDb.Model;
+using DocumentDb.Tools;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentDb.Demos
+{
+    public static class QueriesDemo
+    {
+        public static async Task RunAsync(string[] args)
+        {
+            var client = DocumentDbExtension.CreateClient();
+            var database = await client.GetDatabaseAsync("IoTEvents");
+            var temperatureSamples = await client.GetCollectionAsync(database, "temperatureSamples");
+            temperatureSamples.IndexingPolicy.IncludedPaths.Add(new IncludedPath
+            {
+                Indexes = new System.Collections.ObjectModel.Collection<Index> {
+                    new HashIndex(DataType.String)
+                }
+            });
+
+            var sqlQuerySpec = new SqlQuerySpec{
+                QueryText = "SELECT * FROM templeratureSamples s WHERE s.Location.Name = 'Pordenone'"
+            };
+
+            var query = client.CreateDocumentQuery<TemperatureSample>(temperatureSamples.DocumentsLink, sqlQuerySpec);
+            var items = query.ToList();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using DocumentDb.Demos;
+
+namespace DocumentDb
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await InsertDemo.RunAsync(args);
+            // await AttachmentsDemo.RunAsync(args);
+            // await DaemonDemo.RunAsync(args);
+            // await CollectionsDemo.RunAsync(args);
+            // await QueriesDemo.RunAsync(args);
+            // await JoinDemo.RunAsync(args);
+            // await RegionUserDefinedFunctionDemo.RunAsync(args);
+            // await CountStoredProcedureDemo.RunAsync(args);
+            // await NormalizeDocumentTriggerDemo.RunAsync(args);
+            // await ClientSidePartitionsDemo.RunAsync(args);
+        }
+    }
+}
